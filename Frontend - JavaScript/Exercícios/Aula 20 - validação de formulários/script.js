0b2515cfefa2c95ced5bb41a48643a98cb68d5b5
Ricardo Jeferson da Silva Francisco <122450316+totorourbem@users.noreply.github.com>
2023-12-29 16:32:41 -0300

    Update da lição de JS
    implementação de mais controles de teclado e campos, além do comportamento do botão submit

--- Frontend - JavaScript/Exercícios/Aula 20 - validação de formulários/script.js
+++ Frontend - JavaScript/Exercícios/Aula 20 - validação de formulários/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    // FUNÇOES GENÉRICAS
+    // FUNÇOES GERAIS
     //popup de campo obrigatório - as vezes é necessário desativar o autiocoplete dos campos para funcionar
-    function popupCampo (input, label) {
+    function togglePopup (input, label) {
         //ocultar popup de campo obrigatório
         input.addEventListener("focus", () => {
             label.classList.add("required-popup");
@@ -13,26 +13,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });    
     };
 
+    function estilizarInputCorreto(input, helper) {
+        helper.classList.remove("visible");
+        input.classList.remove("error");
+        input.classList.add("correct");
+    }
+
+    function estilizarInputIncorreto(input, helper) {
+        helper.classList.add("visible");
+        input.classList.add("error");
+        input.classList.remove("correct");
+    }
     //VALIDAÇÃO USERNAME
     // Variáveis
     let usernameInput = document.getElementById("username");
     let usernameLabel = document.querySelector('label[for="username"]');
     let usernameHelper = document.getElementById("username-helper");
 
-    popupCampo(usernameInput, usernameLabel);
+    togglePopup(usernameInput, usernameLabel);
 
     //validar valor dos input
     usernameInput.addEventListener("change", function(e){
         let valor = e.target.value
         if (valor.length < 5) {
-            usernameInput.classList.remove("correct")
-            usernameInput.classList.add("error")
             usernameHelper.innerText = "O username precisa ter 5 ou mais caracteres"
-            usernameHelper.classList.add("visible")
+            estilizarInputIncorreto(usernameInput, usernameHelper)
+            inputsCorretos.username = false
         } else{
-            usernameInput.classList.remove("error")
-            usernameInput.classList.add("correct")
-            usernameHelper.classList.remove("visible")
+            estilizarInputCorreto(usernameInput, usernameHelper)
+            inputsCorretos.username = true
         }
     });
 
@@ -42,22 +51,81 @@ document.addEventListener("DOMContentLoaded", function() {
     let emailLabel = document.querySelector('label[for="email"]');
     let emailHelper = document.getElementById("email-helper");
     
-    popupCampo(emailInput, emailLabel);    
+    togglePopup(emailInput, emailLabel);    
     
     //validar valores dos input
     emailInput.addEventListener("change", function(e){
         let valor = e.target.value
         if(valor.includes("@") && valor.includes(".com")) {
-            emailInput.classList.remove("error")
-            emailInput.classList.add("correct")
-            emailHelper.classList.remove("visible")
+            estilizarInputCorreto(emailInput, emailHelper)
+            inputsCorretos.email = true
         }else {
-            emailInput.classList.remove("correct")
-            emailInput.classList.add("error")
-            emailHelper.innerText = "Por fgavor, insira um e-mail válido."
-            emailHelper.classList.add("visible")
+            emailHelper.innerText = "Por favor, insira um e-mail válido."
+            estilizarInputIncorreto(emailInput, emailHelper)
+            inputsCorretos.email = false
         }
 
-    }
-    )})
+    });
+    
+    //VALIDAÇÃO SENHA
+    // Variáveis
+    let senhaInput = document.getElementById("senha");
+    let senhaLabel = document.querySelector('label[for="senha"]');
+    let senhaHelper = document.getElementById("senha-helper"); 
+    
+    togglePopup(senhaInput, senhaLabel);
 
+    //validar valores dos input
+    senhaInput.addEventListener("blur", function(e){
+        let valor = e.target.value
+        if (valor==="") {
+            senhaHelper.innerText = "O campo senha não pode estar vazio."
+            estilizarInputIncorreto(senhaInput, senhaHelper)
+            inputsCorretos.senha = false
+        } else{
+            estilizarInputCorreto(senhaInput, senhaHelper)
+            inputsCorretos.senha = true
+        }
+    });
+
+    //VALIDAÇÃO CONFIRMA-SENHA
+    // Variáveis
+    let confirmaSenhaInput = document.getElementById("confirma-senha");
+    let confirmaSenhaLabel = document.querySelector('label[for="confirma-senha"]');
+    let confirmaSenhaHelper = document.getElementById("confirma-senha-helper"); 
+    
+    togglePopup(confirmaSenhaInput, confirmaSenhaLabel);
+
+    //validar valores dos input
+    confirmaSenhaInput.addEventListener("change", function(e){
+        let confirmaSenhaValor = e.target.value
+        if (confirmaSenhaValor === senhaInput.value) {
+            estilizarInputCorreto(confirmaSenhaInput, confirmaSenhaHelper);
+            inputsCorretos.confirmaSenha = true
+        } else{
+            confirmaSenhaHelper.innerText = "O campo confirmar senha deve ser identico ao campo senha."
+            estilizarInputIncorreto(confirmaSenhaInput, confirmaSenhaHelper);
+            inputsCorretos.confirmaSenha = false
+        }
+    });
+
+    //CONTROLE DE ENVIO DE FORMULÁRIO
+
+    let btnSubmit = document.querySelector("button[type='submit']");
+    let inputsCorretos = {
+        username: false,
+        email: false,
+        senha: false,
+        confirmaSenha: false
+    }
+
+    btnSubmit.addEventListener("click", (e)=>{
+        if(inputsCorretos.username == false || inputsCorretos.email == false || inputsCorretos.senha == false || inputsCorretos.confirmaSenha == false){
+            e.preventDefault()
+            alert("Os campos obrigatórios precisam ser preenchidos corretamente!")
+        }else{
+            alert("Formulário enviado com sucesso!")
+        }
+    })
+
+})
